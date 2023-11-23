2a3d877b753367728b4a5f82a6444e8de77d4da3
Lex <lex.ansems1991@gmail.com>
2023-11-23 11:28:31 +0100

    Changed the website message, cleared todos.txt

--- web.py
+++ web.py
@@ -10,8 +10,8 @@ def add_todo():
 
 
 st.title("My Todo App")
-st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity")
+st.subheader("These are the things you still need to do.")
+st.write("To complete/remove an item from the list click on the checkbox.")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
